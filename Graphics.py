59dad46c94bcc698ef0a288b24b9228c35363fb7
Armody <Rmdyzie@gmail.com>
2024-11-19 15:29:33 +0300

    added draw_move

--- Graphics.py
+++ Graphics.py
@@ -65,4 +65,16 @@ class Cell():
         if self.has_bottom_wall:
             bottom_wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
             self.__win.draw_line(bottom_wall)
+
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            color="gray"
+        else:
+            color="red"
+        self_mid_x = self.__x1 + (self.__x2 - self.__x1) / 2
+        self_mid_y = self.__y1 + (self.__y2 - self.__y1) / 2
+        to_cell_mid_x = to_cell.__x1 + (to_cell.__x2 - to_cell.__x1) / 2
+        to_cell_mid_y = to_cell.__y1 + (to_cell.__y2 - to_cell.__y1) / 2
+        path = Line(Point(self_mid_x, self_mid_y), Point(to_cell_mid_x, to_cell_mid_y))
+        self.__win.draw_line(path, color)
         
--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from Graphics import *
 
 def main():
     win = Window(800, 600)
-    cell = Cell(Point(10,10), Point(100, 100), win)
-    cell.draw()
+    c1 = Cell(Point(10,10), Point(100, 100), win)
+    c2 = Cell(Point(10, 100), Point(100, 200), win)
+    c1.has_bottom_wall = False
+    c1.draw()
+    c2.has_top_wall = False
+    c2.has_right_wall = False
+    c2.draw()
+    c1.draw_move(c2)
     win.wait_for_close()
 
 main()
