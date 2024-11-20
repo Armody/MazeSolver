4579dca36b779d8746acfa4e794d4d9dd2446d98
Armody <Rmdyzie@gmail.com>
2024-11-20 14:53:07 +0300

    added maze drawing

--- Cell.py
+++ Cell.py
@@ -1,41 +1,48 @@
 from Graphics import *
 
-
 class Cell():
-    def __init__(self, p1, p2, window):
+    def __init__(self, win):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.__x1 = p1.x
-        self.__x2 = p2.x
-        self.__y1 = p1.y
-        self.__y2 = p2.y
-        self.__win = window
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
 
-    def draw(self):
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
         if self.has_left_wall:
-            left_wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
-            self.__win.draw_line(left_wall)
+            left_wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(left_wall)
         if self.has_right_wall:
-            right_wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
-            self.__win.draw_line(right_wall)
+            right_wall = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(right_wall)
         if self.has_top_wall:
-            top_wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
-            self.__win.draw_line(top_wall)
+            top_wall = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(top_wall)
         if self.has_bottom_wall:
-            bottom_wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
-            self.__win.draw_line(bottom_wall)
+            bottom_wall = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(bottom_wall)
 
     def draw_move(self, to_cell, undo=False):
+        color="red"
         if undo:
             color="gray"
-        else:
-            color="red"
-        self_mid_x = self.__x1 + (self.__x2 - self.__x1) / 2
-        self_mid_y = self.__y1 + (self.__y2 - self.__y1) / 2
-        to_cell_mid_x = to_cell.__x1 + (to_cell.__x2 - to_cell.__x1) / 2
-        to_cell_mid_y = to_cell.__y1 + (to_cell.__y2 - to_cell.__y1) / 2
+            
+        self_mid_x = self._x1 + abs(self._x2 - self._x1) / 2
+        self_mid_y = self._y1 + abs(self._y2 - self._y1) / 2
+
+        to_cell_mid_x = to_cell._x1 + (to_cell._x2 - to_cell._x1) / 2
+        to_cell_mid_y = to_cell._y1 + (to_cell._y2 - to_cell._y1) / 2
+
         path = Line(Point(self_mid_x, self_mid_y), Point(to_cell_mid_x, to_cell_mid_y))
-        self.__win.draw_line(path, color)
+        self._win.draw_line(path, color)
         
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from Graphics import *
-from Cell import *
+from Maze import *
 
 def main():
     win = Window(800, 600)
-    c1 = Cell(Point(10,10), Point(100, 100), win)
-    c2 = Cell(Point(10, 100), Point(100, 200), win)
-    c1.has_bottom_wall = False
-    c1.draw()
-    c2.has_top_wall = False
-    c2.has_right_wall = False
-    c2.draw()
-    c1.draw_move(c2)
+    maze = Maze(10, 10, 4, 5, 50, 60, win)
+
     win.wait_for_close()
 
 main()
